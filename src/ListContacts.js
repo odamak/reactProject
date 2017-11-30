3b8f2d4101bad414e50ba474f002ed8176b11a60
omar <omardamak1@gmail.com>
2017-11-30 08:37:22 +0000

    Add object destructuring for this.props.contacts and this.state.query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,12 +18,16 @@ class ListContacts extends Component {
 	}
 
 	render() {
+
+		const { contacts, onDeleteContact } = this.props
+		const { query } = this.state
+
 		let showingContacts
-		if (this.state.query) {
-			const match = new RegExp(escapeRegExp(this.state.query), 'i')
-			showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingContacts = contacts.filter((contact) => match.test(contact.name))
 		} else {
-			showingContacts = this.props.contacts
+			showingContacts = contacts
 		}
 
 		showingContacts.sort(sortBy('name'))
@@ -36,7 +40,7 @@ class ListContacts extends Component {
 						className='search-contacts'
 						type='text'
 						placeholder='Search contacts'
-						value={this.state.query}
+						value={query}
 						onChange={(event) => this.updateQuery(event.target.value)}
 					/>
 				</div>
