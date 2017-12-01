73e68f0be2ea6611dd22913421a6c2bc25f7cb1c
omar <omardamak1@gmail.com>
2017-12-01 19:06:57 +0000

    Add contact works completly

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ class App extends Component {
     ContactsAPI.remove(contact)
 
   }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
@@ -37,10 +46,16 @@ class App extends Component {
 
           )}/>
 
-        <Route path="/create" component={CreateContact}/>
+        <Route path="/create" render={( { history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
-
-    )
+      )
   }
 
 }
