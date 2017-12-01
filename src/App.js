99c53ebd1dead37a39aa1c853300e21f0aec8038
omar <omardamak1@gmail.com>
2017-12-01 10:06:26 +0000

    Simulate react router with states and no hard coding

--- src/App.js
+++ src/App.js
@@ -33,6 +33,9 @@ class App extends Component {
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
+            onNavigate={() => {
+              this.setState({ screen: 'create'})
+            }}
           />
         )}
         {this.state.screen === 'create' && (
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -46,7 +46,12 @@ class ListContacts extends Component {
 						placeholder='Search contacts'
 						value={query}
 						onChange={(event) => this.updateQuery(event.target.value)}
-						/>
+					/>
+					<a
+						href="#create"
+						onClick={this.props.onNavigate}
+						className="add-contact"
+					>Add Contact</a>
 				</div>
 
 				{showingContacts.length !== contacts.length && (
@@ -76,33 +81,5 @@ class ListContacts extends Component {
 			)
 	}
 }
-
-// function ListContacts (props) {
-// 		//console.log('Props', this.props)
-// 		return(
-// 			<ol className='contact-list'>
-// 			{props.contacts.map((contact) => (
-// 				<li key={contact.id} className='contact-list-item'>
-// 					<div className='contact-avatar' style={{
-// 						backgroundImage: `url(${contact.avatarURL})`
-// 					}}/>
-// 					<div className='contact-details'>
-// 						<p>{contact.name}</p>
-// 						<p>{contact.email}</p>
-// 					</div>
-// 					<button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
-// 					Remove
-// 					</button>
-// 				</li>
-
-// 				))}
-// 			</ol>
-// 		)
-// }
-
-// ListContacts.propTypes = {
-// 	contacts: PropTypes.array.isRequired,
-// 	onDeleteContact: PropTypes.func.isRequired
-// }
 
 export default ListContacts
